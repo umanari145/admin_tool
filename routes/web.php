f9f9df7dd1793d153dbb0555c0b66dbd7e136501
umanari145 <umanari145@gmail.com>
2022-03-12 06:36:46 +0900

    laravelのrouteを設定

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'api'], function () {
     Route::get('csv_category/{csv_category}', "Api\CsvController@getCsvField")->name('getCsvField');
     Route::post('csv_category/{csv_category}', "Api\CsvController@registCsvField")->name('registCsvField');
     Route::put('csv_field/{csv_id}', "Api\CsvController@updateCsvField")->name('updateCsvField');
     Route::delete('csv_field', "Api\CsvController@deleteCsvField")->name('deleteCsvField');
 });
+
+
+// vue対策でどんなURLできてもwelcomeに行くように
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any','.*');
