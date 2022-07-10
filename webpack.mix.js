2fe4c9bfaad22fffc4effec8b7a75d9ba19ccb29
umanari145 <umanari145@gmail.com>
2022-07-10 21:39:34 +0900

    npmのバージョン修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     // watchには使わない
     //.copy('node_modules/@coreui/icons/svg', 'public/icons/svg')
     .sass('resources/sass/app.scss', 'public/css')
