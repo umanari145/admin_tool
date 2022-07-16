9aa18f31c77870fdf95c270be138db60b99a46b7
umanari145 <umanari145@gmail.com>
2022-07-16 22:50:34 +0900

    encrypterを仮実装

---
+++ tests/Util/CustomEncrypterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Service;
+
+use Tests\TestCase;
+use App\Form\FormValid;
+
+class CustomEncrypterTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+
+    }
+}
